54406d389c997454f05f49311c2efffa3a8a5358
Zoltan Temesvari <zoltan.temesvari@outlook.com>
2023-05-16 15:34:36 +0200

    custom errors

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Raitweet
+
+This is a P2P meesaging system to complement Nano Node. WIP.
--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,61 @@ use std::sync::{Mutex};
 
 #[derive(Deserialize, Serialize)]
 struct TransactionData {
-    transaction: String,
-    sender_account: String,
+    send_block: String,
     payload: String,
+    payload_hash: String,
     signature: String,
 }
 
 #[derive(Deserialize, Serialize)]
 struct ReadRequest {
-    transaction: String,
-    sender_account: String,
+    receive_block: String,
+    send_block: String,
     signature: String,
 }
 
+#[derive(Deserialize, Serialize)]
+struct ReadResponse {
+    payload: String
+}
+
+#[derive(Deserialize, Serialize)]
+
+enum CustomHttpResponse {
+    InvalidForm,
+    InvalidSignature,
+    TransactionNotFound,
+    TransactionProcessed,
+    GeneralError
+}
+
 type Transactions = Mutex<HashMap<String, TransactionData>>;
+
+fn custom_http_response(response: CustomHttpResponse) -> HttpResponse {
+    match response {
+        CustomHttpResponse::InvalidForm => HttpResponse::BadRequest().body("Invalid form or too large"),
+        CustomHttpResponse::InvalidSignature => HttpResponse::Unauthorized().body("Invalid signature"),
+        CustomHttpResponse::TransactionNotFound => HttpResponse::NotFound().body("Transaction not found"),
+        CustomHttpResponse::TransactionProcessed => HttpResponse::Created().body("Transaction processed"),
+        CustomHttpResponse::GeneralError => HttpResponse::InternalServerError().body("General error"),
+    }
+}
 
 async fn process_transaction(
     transaction_data: web::Json<TransactionData>,
     transactions: web::Data<Transactions>,
 ) -> impl Responder {
     if !validate_form(&transaction_data) {
-        return HttpResponse::BadRequest().body("Invalid form or too large");
+        return custom_http_response(CustomHttpResponse::InvalidForm)
     }
 
     if !validate_signature(&transaction_data) {
-        return HttpResponse::BadRequest().body("Invalid signature");
+        return custom_http_response(CustomHttpResponse::InvalidSignature)
     }
 
     let mut transactions: std::sync::MutexGuard<HashMap<String, TransactionData>> = transactions.lock().unwrap();
-    transactions.insert(transaction_data.transaction.clone(), transaction_data.into_inner());
-    HttpResponse::Ok().body("Transaction processed")
+    transactions.insert(transaction_data.send_block.clone(), transaction_data.into_inner());
+    custom_http_response(CustomHttpResponse::TransactionProcessed)
 }
 
 async fn read_transaction(
@@ -42,24 +67,29 @@ async fn read_transaction(
     transactions: web::Data<Transactions>,
 ) -> impl Responder {
     if !validate_signature(&read_request) {
-        return HttpResponse::BadRequest().body("Invalid signature");
+        return custom_http_response(CustomHttpResponse::InvalidSignature)
     }
 
     let transactions: std::sync::MutexGuard<HashMap<String, TransactionData>> = transactions.lock().unwrap();
-    match transactions.get(&read_request.transaction) {
+    match transactions.get(&read_request.send_block) {
         Some(transaction_data) => {
+            let transaction_data = ReadResponse {
+                payload: transaction_data.payload.clone(),
+            };
             HttpResponse::Ok().json(transaction_data)
         }
-        None => HttpResponse::NotFound().body("Transaction not found"),
+        None => custom_http_response(CustomHttpResponse::TransactionNotFound)
     }
 }
 
 fn validate_form(transaction_data: &TransactionData) -> bool {
-    let json_size = serde_json::to_string(transaction_data).unwrap().len();
-
-    // Check overall size (max. 512 bytes)
-    if json_size > 512 {
-        return false;
+    let data_json = serde_json::to_string(transaction_data);
+    match data_json {
+        Ok(data) =>  if data.len() > 512 {
+                                    return false;
+                                    }
+                  else {()}
+        Err(_) => return false
     }
 
     // Add further form validation here if needed
@@ -85,8 +115,8 @@ async fn main() -> std::io::Result<()> {
         
         App::new()
             .app_data(transactions.clone())
-            .service(web::resource("/transaction").route(web::post().to(process_transaction)))
-            .service(web::resource("/read").route(web::post().to(read_transaction)))
+            .service(web::resource("/send").route(web::post().to(process_transaction)))
+            .service(web::resource("/receive").route(web::post().to(read_transaction)))
     })
     .bind("127.0.0.1:8080")?
     .run()
